5048d00e4373e4f1dd2468f220fe31e4b380dbd8
dvrana <dylanvrana@gmail.com>
2016-09-12 18:52:49 -0400

    Added case projection to SamplePCA

--- src/main/scala/org/broadinstitute/hail/methods/SamplePCA.scala
+++ src/main/scala/org/broadinstitute/hail/methods/SamplePCA.scala
@@ -2,8 +2,8 @@ package org.broadinstitute.hail.methods
 
 import org.apache.spark.mllib.linalg.{Matrix, DenseMatrix}
 import org.apache.spark.rdd.RDD
-import org.broadinstitute.hail.variant.Variant
-import org.broadinstitute.hail.variant.VariantDataset
+import org.broadinstitute.hail.annotations._
+import org.broadinstitute.hail.variant._
 
 class SamplePCA(k: Int, computeLoadings: Boolean, computeEigenvalues: Boolean) {
   def name = "SamplePCA"
@@ -33,5 +33,15 @@ class SamplePCA(k: Int, computeLoadings: Boolean, computeEigenvalues: Boolean) {
         None
 
     (scores, loadings, eigenvalues)
+  }
+
+  // Projects samples into PC loadings
+  def project(vds : VariantDataset, loadings : Map[Variant, Array[Double]]) : RDD[(String,Array[Double])] = {
+    (vds.filterVariants((v : Variant,_,_) => loadings contains v)
+      .aggregateBySampleWithKeys(Array.fill[Double](k)(0.0))(
+        (pos : Array[Double], v : Variant, _ : String, gt : Genotype) =>
+          Array.tabulate(k)((i : Int) => pos(i) + (loadings(v)(i) * gt.nNonRefAlleles.get)),
+        (pos1 : Array[Double], pos2 : Array[Double]) => Array.tabulate(k)((i : Int) => pos1(i) + pos2(i))
+    ))
   }
 }
